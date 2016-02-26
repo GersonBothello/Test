0f8b7312b1f61bc6bb321aba3d5d4e9789fb2861
unknown <M671856@L4692185.federated.fds>
2016-02-26 13:30:20 -0800

    test 2

---
+++ WorkflowEventData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class WorkflowEventData
+    {
+        public DateTime date { get; set; }
+        public string name { get; set; }
+        public string user { get; set; }
+        public string rolename { get; set; }
+        public string task { get; set; }
+        public string taskname { get; set; }
+        public string other { get; set; }
+        public string othername { get; set; }
+    }
+}
