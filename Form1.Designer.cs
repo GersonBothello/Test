dd217a4c2747581bd3fee8e9d4b43f5a0f4f8274
unknown <M671856@L4692185.federated.fds>
2016-02-26 11:37:40 -0800

    Test Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(53, 49);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(610, 20);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Folder ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button2.Location = new System.Drawing.Point(670, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(53, 110);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(610, 20);
+            this.txtDestination.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Destination";
+            // 
+            // txtError
+            // 
+            this.txtError.Location = new System.Drawing.Point(56, 167);
+            this.txtError.Multiline = true;
+            this.txtError.Name = "txtError";
+            this.txtError.ReadOnly = true;
+            this.txtError.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtError.Size = new System.Drawing.Size(607, 61);
+            this.txtError.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Error Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 269);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Astra Pending Workflows XML Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtError;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        const string folderPath = @"C:\_Projects\WindowsFormsApplication1\Active_workflows\Active_workflows\";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folderPath = txtFolderPath.Text;
+
+                if (!string.IsNullOrEmpty(folderPath))
+                {
+                    GenerateExcelFile createFiles = new GenerateExcelFile();
+                   var errorFiles = createFiles.CreateFiles(folderPath);
+                   txtDestination.Text = folderPath + "\\" + "workflow.csv";
+
+
+                   string errorFile = string.Empty;
+                   foreach (string item in errorFiles)
+                   {
+                       errorFile = item + System.Environment.NewLine;
+                   }
+
+                   txtError.Text = errorFile;
+                }
+
+                MessageBox.Show("Complete");
+            
+            }
+            catch (Exception ex)
+            {
+                txtError.Text = ex.Message;
+            }
+        }
+
+        private void GetData()
+        {
+
+            var xmlFiles = System.IO.Directory.GetFiles(folderPath + "\\fixFiles\\");
+            List<WorkflowData> workflowList = new List<WorkflowData>();
+
+            foreach (string file in xmlFiles)
+            {
+                GeFileData(file, workflowList);
+            }
+
+            WriteWorkFlowFile(workflowList);
+
+        }
+
+        private void FixFile()
+        {
+
+            var xmlFiles = System.IO.Directory.GetFiles(folderPath);
+
+            foreach (string file in xmlFiles)
+            {
+                fixXml(file);
+            }
+
+        }
+
+
+        private void fixXml(string filePath)
+        {
+            List<string> writeLines = new List<string>();
+
+            var fileName = System.IO.Path.GetFileName(filePath);
+
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line != "</workflow>")
+                    {
+                        writeLines.Add(line);
+                    }
+                }
+
+                writeLines.Add("</workflow>");
+            }
+
+            if (!System.IO.Directory.Exists(folderPath + "\\fixFiles\\"))
+            {
+                System.IO.Directory.CreateDirectory(folderPath + "\\fixFiles\\");
+            }
+
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(folderPath + "\\fixFiles\\" + fileName))
+            {
+                foreach (string item in writeLines)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+        }
+
+
+
+
+        public string GeFileData(string filePath, List<WorkflowData> workflowList)
+        {
+
+            string returnValue = string.Empty;
+
+            List<WorkflowEventData> eventList = new List<WorkflowEventData>();
+
+            System.Xml.Linq.XDocument xDoc = System.Xml.Linq.XDocument.Load(filePath);
+
+            var result = xDoc.Descendants("workflow");
+
+
+            foreach (var item in result)
+            {
+                var name = item.Attribute("name").Value;
+                var active = item.Attribute("active").Value;
+                var owner = item.Attribute("owner").Value;
+                var creator = item.Attribute("creator").Value;
+                var activationtime = FromUnixTime(item.Attribute("activationtime").Value);
+                var description = item.Descendants("description").First().Value;
+
+                var jobId = item.Descendants("jobid").First().Value;
+                var row = new WorkflowData();
+
+                var lastTask = item.Descendants("lasttask").First().Value;
+
+                string[] splitLastTask = lastTask.Split('\n');
+
+                foreach (string taskItem in splitLastTask)
+                {
+                    string[] colonSplit = taskItem.Split(':');
+
+                    if (colonSplit[0].Equals("Taskname", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        row.lasttask = colonSplit[1];
+                    }
+
+                    if (colonSplit[0].Equals("Description", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        row.TaskDescription = colonSplit[1];
+                    }
+
+                }
+
+                if (creator.Equals("yc19d1s", StringComparison.CurrentCultureIgnoreCase) || creator.Equals("w003spj", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    continue;
+                }
+
+                row.name = name;
+                row.active = active;
+                row.owner = owner;
+                row.creator = creator;
+                row.activationtime = activationtime;
+                row.description = description;
+                row.JobId = jobId;
+                //row.lasttask = lastTask;
+
+                var workflowEvent = item.Descendants("event");
+
+                foreach (var eventItem in workflowEvent)
+                {
+                    WorkflowEventData eventrow = new WorkflowEventData();
+
+                    eventrow.date = FromUnixTime(eventItem.Attribute("date").Value);
+                    eventrow.name = eventItem.Attribute("name").Value;
+                    eventrow.user = eventItem.Attribute("user").Value;
+                    eventrow.rolename = eventItem.Attribute("rolename").Value;
+                    eventrow.task = eventItem.Attribute("task").Value;
+                    eventrow.taskname = eventItem.Attribute("taskname").Value;
+                    eventrow.other = eventItem.Attribute("other").Value;
+                    eventrow.othername = eventItem.Attribute("othername").Value;
+
+                    eventList.Add(eventrow);
+
+                }
+
+                var modifiedDate = (from d in eventList select d.date).Max();
+
+                var lastEventRow = eventList.LastOrDefault();
+
+                row.LastModified = modifiedDate;
+
+                DateTime startTime = Convert.ToDateTime("01/06/2016");
+
+                row.DaysLastModified = (startTime - modifiedDate.Date).Days;
+
+                workflowList.Add(row);
+            }
+
+
+
+
+            return returnValue;
+        }
+
+
+        private void WriteWorkFlowFile(List<WorkflowData> listData)
+        {
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(folderPath + "workflow.csv");
+            string comma = ",";
+
+            string header = "name" + comma + "active" + comma + "owner" + comma + "creator" + comma + "activationtime" + comma + "description" + comma + "Job Id" + comma + "Last Modified" + comma + "Last Task" + comma + "Task Description" + comma + "Days Since Last Modified " + DateTime.Today.ToString();
+            writer.WriteLine(header);
+
+            foreach (WorkflowData item in listData)
+            {
+                string row = item.name + comma + item.active + comma + item.owner + comma + item.creator + comma + item.activationtime.ToString() + comma + item.description + comma + item.JobId + comma + item.LastModified + comma + item.lasttask + comma + item.TaskDescription + comma + item.DaysLastModified;
+                writer.WriteLine(row);
+            }
+
+            writer.Close();
+            writer.Dispose();
+        }
+
+
+        private void WriteEventFile(List<WorkflowEventData> listData)
+        {
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:\Personal\Macys\Active Jobs\Active Jobs\workflowEvent.csv");
+            string comma = ",";
+
+            string header = "date" + comma + "name" + comma + "user" + comma + "rolename" + comma + "task" + comma + "taskname" + comma + "other" + comma + "othername";
+            writer.WriteLine(header);
+
+            foreach (WorkflowEventData item in listData)
+            {
+                string row = item.date.ToString() + comma + item.name + comma + item.user + comma + item.rolename + comma + item.task + comma + item.taskname + comma + item.other + comma + item.othername;
+                writer.WriteLine(row);
+            }
+
+            writer.Close();
+            writer.Dispose();
+        }
+
+
+        public DateTime FromUnixTime(string unixTimeString)
+        {
+
+            long unixTime = long.Parse(unixTimeString);
+
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+
+        public static string GetRootImgAlt(System.Xml.Linq.XDocument xDoc, string nodeName)
+        {
+            string returnValue = string.Empty;
+            var result = xDoc.Descendants(nodeName);
+
+            if (result != null)
+            {
+                if (result.Count() > 0)
+                {
+                    XElement element = result.First();
+                    if (element != null)
+                    {
+                        if (element.HasElements)
+                        {
+                            //returnValue = element.Descendants(Img).Attributes(Alt).First().Value;
+                        }
+                    }
+                }
+            }
+            return returnValue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            folderBrowser.ShowDialog();
+
+            string folderPath = folderBrowser.SelectedPath;
+
+            txtFolderPath.Text = folderPath;
+            
+            //if (!string.IsNullOrEmpty(folderPath))
+            //{
+            //    GenerateExcelFile createFiles = new GenerateExcelFile();
+            //    createFiles.CreateFiles(folderPath);
+            //}
+        }
+
+    }
+}
