c97cb9755dd8ba5399fa47338d3c27a7b5a0cce9
unknown <M671856@L4692185.federated.fds>
2016-02-26 13:30:06 -0800

    test 1

---
+++ WorkflowData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class WorkflowData
+    {
+        public string name { get; set; }
+        public string active { get; set; }
+        public string owner { get; set; }
+        public string creator { get; set; }
+        public DateTime activationtime { get; set; }
+        public string description { get; set; }
+        public string JobId { get; set; }
+        public string lasttask { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime LastModified { get; set; }
+        public int DaysLastModified { get; set; }
+    }
+}
